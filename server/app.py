2a883548a741ce1ec23f0c007782c0d7150ed3d7
eddy <eddy.mwaniki.27@gmail.com>
2025-03-30 16:36:58 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -19,7 +19,39 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
-# start building your API here
+@app.route("/games")
+def games():
+    games = []
+    for game in Game.query.all():
+      game_dict = game.to_dict()
+      games.append(game_dict)
+    
+    response = make_response(jsonify(games),200)
+
+    return response
+
+@app.route("/games/<int:id>")
+def get_games_by_id(id):
+   game = Game.query.filter(Game.id == id).first()
+   
+   game_dict = game.to_dict()
+
+   response = make_response(jsonify(game_dict), 200)
+
+   return response
+
+@app.route("/games/users/<int:id>")
+def get_users_of_a_game(id):
+    game = Game.query.filter(Game.id == id).first()
+    users = []
+    for review in game.reviews:
+      user = review.user
+      user_dict = user.to_dict(rules=("-reviews",))
+      users.append(user_dict)
+      
+    response = make_response(users, 200)
+    return response  
+
 
 
 if __name__ == '__main__':
